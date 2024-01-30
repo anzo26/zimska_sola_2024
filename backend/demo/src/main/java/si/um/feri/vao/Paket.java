e68c48261633dba3e8f8f488cba06ad863b5dfdf
anzo26 <anze.gol10@gmail.com>
2024-01-30 10:24:47 +0100

    initial backend + paket CRUD

---
+++ backend/demo/src/main/java/si/um/feri/vao/Paket.java
@@ -0,0 +1,46 @@
+package si.um.feri.vao;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import si.um.feri.dto.PaketDto;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Paket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String naziv, tip, opis;
+    private int kolicina_prenosa;
+    private Double cena;
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Paket(PaketDto dto) {
+        setNaziv(dto.naziv());
+        setTip(dto.tip());
+        setOpis(dto.opis());
+        setKolicina_prenosa(dto.kolicina_prenosa());
+        setCena(dto.cena());
+    }
+
+    public void updateFrom(PaketDto dto) {
+        setNaziv(dto.naziv());
+        setTip(dto.tip());
+        setOpis(dto.opis());
+        setKolicina_prenosa(dto.kolicina_prenosa());
+        setCena(dto.cena());
+    }
+
+    public PaketDto toDto() {
+        return new PaketDto(id, naziv, tip, opis, kolicina_prenosa, cena);
+    }
+}
